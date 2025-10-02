f863860f8a434837e93e1b06c5d66fd1837ac162
Daniela Medeiro Mota <danielamedeiromota@hotmail.com>
2025-10-02 16:34:56 -0300

    first commit

---
+++ testsecurity/src/test/java/dev/danimota/testsecurity/TestsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danimota.testsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
